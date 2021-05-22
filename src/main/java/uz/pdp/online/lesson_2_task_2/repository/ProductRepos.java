b721bf22675c6a65f6c3e1b1845dc51164b37608
BaxtiyorMuhammadaliyev <baxtiyor.08042000@gmail.com>
2021-05-22 10:56:17 +0500

    secondAttempt

--- src/main/java/uz/pdp/online/lesson_2_task_2/repository/ProductRepos.java
+++ src/main/java/uz/pdp/online/lesson_2_task_2/repository/ProductRepos.java
@@ -1,10 +1,19 @@
 package uz.pdp.online.lesson_2_task_2.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import uz.pdp.online.lesson_2_task_2.entity.Product;
 import uz.pdp.online.lesson_2_task_2.projection.CustomProduct;
 
 @RepositoryRestResource(path = "product", excerptProjection = CustomProduct.class)
 public interface ProductRepos extends JpaRepository<Product, Integer> {
+
+    @RestResource(path = "byPrice")
+    public Page<Product> findAllByPriceBetween(@Param("min_price") Double min, @Param("max_price") Double max, Pageable pageable);
+
+
 }
--- src/main/java/uz/pdp/online/lesson_2_task_2/repository/PropertyRepos.java
+++ src/main/java/uz/pdp/online/lesson_2_task_2/repository/PropertyRepos.java
@@ -1,10 +1,20 @@
 package uz.pdp.online.lesson_2_task_2.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import uz.pdp.online.lesson_2_task_2.entity.Product;
 import uz.pdp.online.lesson_2_task_2.entity.Property;
 import uz.pdp.online.lesson_2_task_2.projection.CustomProperty;
 
 @RepositoryRestResource(path = "property", excerptProjection = CustomProperty.class)
 public interface PropertyRepos extends JpaRepository<Property, Integer> {
+
+    @RestResource(path = "sort")
+    public Page<Product> findAllByCharacteristicsId(@Param("charId") Integer characteristics_id, Pageable pageable);
+
 }
+    
