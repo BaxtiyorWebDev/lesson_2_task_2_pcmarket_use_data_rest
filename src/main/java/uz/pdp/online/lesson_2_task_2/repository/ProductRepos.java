76d62f997b02ff24f340ab8fb6276166a313ac47
BaxtiyorMuhammadaliyev <baxtiyor.08042000@gmail.com>
2021-05-22 12:11:43 +0500

    thirdAttempt

--- src/main/java/uz/pdp/online/lesson_2_task_2/repository/ProductRepos.java
+++ src/main/java/uz/pdp/online/lesson_2_task_2/repository/ProductRepos.java
@@ -3,11 +3,14 @@ package uz.pdp.online.lesson_2_task_2.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import uz.pdp.online.lesson_2_task_2.entity.Product;
 import uz.pdp.online.lesson_2_task_2.projection.CustomProduct;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "product", excerptProjection = CustomProduct.class)
 public interface ProductRepos extends JpaRepository<Product, Integer> {
@@ -15,5 +18,10 @@ public interface ProductRepos extends JpaRepository<Product, Integer> {
     @RestResource(path = "byPrice")
     public Page<Product> findAllByPriceBetween(@Param("min_price") Double min, @Param("max_price") Double max, Pageable pageable);
 
+
+    @Query(value = "select * from product pr join characteristics chr on pr.id=chr.product_id " +
+            "join property pp on pp.characteristics_id=chr.id " +
+            "where in pr.id:=list", nativeQuery = true)
+    List<Product> allByProductList(List<Integer> list);
 
 }
